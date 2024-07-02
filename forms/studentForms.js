7acb8ace0a5464c4cc0988a8f847cd4eb29506b0
Mawuli Agbeko <jmmyagbeko@st.ug.edu.gh>
2024-07-02 05:20:46 -1200

    update

--- forms/studentForms.js
+++ forms/studentForms.js
@@ -42,9 +42,103 @@ const admissionFormSchema = new mongoose.Schema({
         address: { type: String, trim: true },
     },
     hostel: {
-        residentialStudent: { type: String },
-        nonResidentialStudent: { type: String },
+        residentialStudent: { type: Boolean, default: false }, 
+        nonResidentialStudent: { type: Boolean, default: false }, 
     },
-},);
+    //additionalInformation
+    educationalBackground: {
+        highSchoolAttended: {
+            schoolName: { type: String },
+            program: { type: String },
+            period: { type: String },
+        },
+        collegeAttended: {
+            schoolName: { type: String },
+            program: { type: String },
+            period: { type: String },
+        },
+    },
+    endorsement: {
+        name: { type: String },
+        signature: { type: String, trim: true },
+        address: { type: String, trim: true },
+        contact: { type: String, required: true, match: [/^\d{10,15}$/, 'Please enter a valid phone number'] },
+        date: { type: Date, required: true  },
+    },
+    signatureOfApplicant: {
+        signature: { type: String, trim: true },
+        date: { type: Date, required: true },
+    },
+    sessionOfChoice: {
+        regular: { type: Boolean, default: false }, 
+        evening: { type: Boolean, default: false }, 
+        weekend: { type: Boolean, default: false }, 
+    },
+    programDetails: {
+        shortCertificateCourse: {
+        iC3DigitalLiteracy: { type: Boolean, default: false },
+        microsoftOfficeSpecialist: { type: Boolean, default: false },
+        practicalHardwareEngineering: { type: Boolean, default: false },
+        practicalComputerNetworking: { type: Boolean, default: false },
+        practicalGraphicDesign: { type: Boolean, default: false },
+        websiteDevelopment: { type: Boolean, default: false },
+        animationAndMultimedia: { type: Boolean, default: false },
+        pythonProgramming: { type: Boolean, default: false },
+        htmlCssAndJavaScript: { type: Boolean, default: false },
+        certificateInSoftwareDevelopment: { type: Boolean, default: false },
+        certificateInDatabaseDevelopment: { type: Boolean, default: false },
+    },
+    professionalDiplomaCourse: {
+        SystemEngineering: {
+            sixMonths: { type: Boolean, default: false },
+            oneYear: { type: Boolean, default: false },
+        },
+        SoftwareEngineering: {
+            sixMonths: { type: Boolean, default: false },
+            oneYear: { type: Boolean, default: false },
+            //twoYears: { type: Boolean, default: false },
+        },
+        GraphicsAndWebDesign: {
+            sixMonths: { type: Boolean, default: false },
+            oneYear: { type: Boolean, default: false },
+            //twoYears: { type: Boolean, default: false },
+        },
+        DatabaseManagement: {
+            sixMonths: { type: Boolean, default: false },
+            oneYear: { type: Boolean, default: false },
+            //twoYears: { type: Boolean, default: false },
+        },
+    },
+    externalAcademicCertificationCourse: {
+        diplomaInInformationTechnology: {
+            sixMonths: { type: Boolean, default: false },
+            oneYear: { type: Boolean, default: false },
+            //twoYears: { type: Boolean, default: false },
+        },
+        diplomaInOfficeManagement: {
+            sixMonths: { type: Boolean, default: false },
+            oneYear: { type: Boolean, default: false },
+            //twoYears: { type: Boolean, default: false },
+        },
+        diplomaInBusinessAdministration: {
+            sixMonths: { type: Boolean, default: false },
+            oneYear: { type: Boolean, default: false },
+            //twoYears: { type: Boolean, default: false },
+        },
+        diplomaInAccounting: {
+            sixMonths: { type: Boolean, default: false },
+            oneYear: { type: Boolean, default: false },
+            //twoYears: { type: Boolean, default: false },
+        },
+    },
+    
+
+
+
+
+
+
+},
+});
 
 module.exports = mongoose.model('AdmissionForm', admissionFormSchema);
